d0f1e2e813a8fd73c336f2f18e68493cdd7579fb
TMS Web Studio <tmswebstudio@gmail.com>
2026-02-27 09:27:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "aquaflow-plannersl-44964-12066",
+  "appId": "1:171029537975:web:dcbe8a27a7e886e9091932",
+  "apiKey": "AIzaSyAHrMSxQRVFpYYwrrRMQV-z1IhR8CZKwWc",
+  "authDomain": "aquaflow-plannersl-44964-12066.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "171029537975"
+};
